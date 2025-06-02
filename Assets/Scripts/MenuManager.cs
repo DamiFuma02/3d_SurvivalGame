6eb4e27c91329d97e4438b1a03296ca1866ea6cc
DamiFuma02 <damianofumagalli2@gmail.com>
2025-06-03 00:58:24 +0200

    Add MenuSettingsUI different for menu and game.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,9 +1,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+
+
+[Serializable] public class PlayerStats {
+    public int health = 100;
+    public int food = 50;
+    public int water = 0;
+}
+
+//[Serializable] public class PlayerInventory {
+//    public List<InventoryItem> items = new List<InventoryItem>();
+//}
+//[Serializable] InventoryItem  {"itemName", "quantity", "category", ... (other values ???)}
+
+[Serializable] public class GameSettings {
+    public bool survivalMode = true;
+    public int gameVolume = 50; // 0-100
+    public int creaturesVolume = 50; // 0-100
+}
+
+[Serializable] public class GameSaveData {
+    public PlayerStats playerStats = new PlayerStats();
+    //public PlayerInventory playerInventory = new PlayerInventory();
+    public GameSettings gameSettings = new GameSettings();
+    public string saveDateTime; // for saving the date and time of the save
+}
+
+
+public enum GameMode { Survival, Creative}
+
+public enum GameDifficulty { Easy, Normal, Hard }
+
 
 public class MenuManager : MonoBehaviour
 {
@@ -18,20 +51,33 @@ public class MenuManager : MonoBehaviour
         }
     }
 
-
-
+    
     // common components
     private GameObject mainMenuUI;
     private GameObject settingsMenuUI;
     // main menu components
     private GameObject loadGameMenuUI;
+    GameObject gameModeSliderUI;
+    public GameMode gameMode = GameMode.Survival;
+    GameObject difficultySliderUI;
+    public GameDifficulty gameDifficulty = GameDifficulty.Easy;
     // game menu components
+    public Dictionary<string, GameObject> volumeGameObjects = new Dictionary<string, GameObject> {
+        { "MainVolume", null },
+        { "CreatureVolume", null }
+    };
+    public Dictionary<string, int> volumeDictionary = new Dictionary<string, int> {
+        { "MainVolume", 50 }, // 0-100
+        { "CreatureVolume", 50 } // 0-100
+    };
+    
     private GameObject saveGameMenuUI;
     
     bool gameLoaded;
     private string mainMenuSceneName = "MainMenuScene";
     private string gameSceneName = "SampleScene";
     public bool isMenuOpen = false;
+    private string menuManager2DIconsPath = "2D_Prefabs/MenuManager/";
 
     // Start is called before the first frame update
     void Start()
@@ -43,13 +89,29 @@ public class MenuManager : MonoBehaviour
 
     private void GetAllMenuUIs() {
         mainMenuUI = transform.Find("MainMenuUI").gameObject;
-        settingsMenuUI = transform.Find("SettingsUI").gameObject;
+        settingsMenuUI = transform.GetChild(2).gameObject;
         if (gameLoaded) {
-            saveGameMenuUI = transform.Find("SaveGameUI").gameObject;
+            volumeGameObjects["MainVolume"] = settingsMenuUI.transform.GetChild(1).GetChild(1).gameObject;
+            volumeGameObjects["MainVolume"].transform.GetChild(1).GetComponent<Slider>().onValueChanged.AddListener((value) => SetVolume(value, "MainVolume"));
+            volumeGameObjects["CreatureVolume"] = settingsMenuUI.transform.GetChild(1).GetChild(2).gameObject;
+            volumeGameObjects["CreatureVolume"].transform.GetChild(1).GetComponent<Slider>().onValueChanged.AddListener((value) => SetVolume(value, "CreatureVolume"));
+            saveGameMenuUI = transform.GetChild(1).gameObject;
         } else {
-            loadGameMenuUI = transform.Find("LoadGameUI").gameObject;
+            gameModeSliderUI = settingsMenuUI.transform.GetChild(1).GetChild(1).GetChild(1).gameObject;
+            gameModeSliderUI.GetComponent<Slider>().onValueChanged.AddListener((value) => SetGameMode(value));
+            difficultySliderUI = settingsMenuUI.transform.GetChild(1).GetChild(2).GetChild(1).gameObject;
+            difficultySliderUI.GetComponent<Slider>().onValueChanged.AddListener((value) => SetGameDifficulty(value));
+            loadGameMenuUI = transform.GetChild(1).gameObject;
         }
     }
+
+    void SetVolume(float value,string volumeKey) {
+        volumeDictionary[volumeKey] = Mathf.RoundToInt(value * 100);
+        string currVolumeIconName = volumeDictionary[volumeKey] == 0 ? "mute_icon" : volumeDictionary[volumeKey] <= 50 ? "lowvolume_icon" : "highvolume_icon";
+        volumeGameObjects[volumeKey].transform.GetChild(0).gameObject.GetComponent<Image>().overrideSprite = Resources.Load<Sprite>(menuManager2DIconsPath + currVolumeIconName);
+    }
+
+
 
     private void GetAllButtons() {
         mainMenuUI.transform.Find("SettingsButton").GetComponent<Button>().onClick.AddListener(OnSettingsButtonClicked);
@@ -64,10 +126,21 @@ public class MenuManager : MonoBehaviour
             loadGameMenuUI.transform.Find("ExitButton").GetComponent<Button>().onClick.AddListener(() => OnBackButtonClicked(loadGameMenuUI));
             mainMenuUI.transform.Find("NewGameButton").GetComponent<Button>().onClick.AddListener(OnNewGameButtonClicked);
         }
-
     }
 
     
+
+
+    void SetGameDifficulty(float value) {
+        gameDifficulty = (GameDifficulty)Mathf.RoundToInt(value);
+    }
+
+
+    void SetGameMode(float value) {
+        gameMode = (GameMode)Mathf.RoundToInt(value);
+        Debug.Log("Game Mode changed to: " + gameMode.ToString());
+        gameModeSliderUI.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = gameMode.ToString();
+    }
 
     private void SaveAndExitSettings() {
         if (gameLoaded) {
@@ -103,6 +176,9 @@ public class MenuManager : MonoBehaviour
         // Add logic to save a game
         saveGameMenuUI.SetActive(true);
         mainMenuUI.SetActive(false);
+        // Create a SaveGameData object and populate it with the current game state
+        GameSaveData saveData = new GameSaveData();
+        saveData.saveDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
     }
     public void OnSettingsButtonClicked() {
         Debug.Log("Settings button clicked");
